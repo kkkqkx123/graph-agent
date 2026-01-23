f8e8ae7771d00453c8d4f329cc35c8a701d5d1e8
Admin <3353074868@qq.com>
2026-01-23 15:43:48 +0800

    rm web

--- src/infrastructure/common/index.ts
+++ src/infrastructure/common/index.ts
@@ -1,12 +1,6 @@
 /**
  * 共享基础设施索引
  */
-// 工具集
-export {
-  ValidationUtils,
-  ValidationResult as CommonValidationResult,
-  ValidationRule as CommonValidationRule,
-} from './utils/validation-utils';
 
 // 不可变状态更新工具
 export {
